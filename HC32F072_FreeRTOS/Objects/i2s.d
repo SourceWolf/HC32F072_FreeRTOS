50b671f18ae820548ea902bc92f9a31bab5efab8
SourceWolf <snowwolf726@163.com>
2019-07-24 16:11:19 +0800

    update MDK setting

--- HC32F072_FreeRTOS/Objects/i2s.d
+++
@@ -1,8 +0,0 @@
-.\objects\i2s.o: driver\src\i2s.c
-.\objects\i2s.o: .\driver\inc\i2s.h
-.\objects\i2s.o: .\mcu\common\hc32f072.h
-.\objects\i2s.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\core_cm0plus.h
-.\objects\i2s.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\i2s.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\i2s.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\i2s.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_armcc.h
--- HC32F072_FreeRTOS/Objects/startup_hc32f072.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_hc32f072.o: mcu\MDK\startup_hc32f072.s
