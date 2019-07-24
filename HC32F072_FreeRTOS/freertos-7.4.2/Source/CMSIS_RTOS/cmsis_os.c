cf95dcc7fa8f62f88cd31abcdc4bb5b93d74f74c
SourceWolf <snowwolf726@163.com>
2019-07-24 16:47:04 +0800

    edit cmsis_os

--- HC32F072_FreeRTOS/freertos-7.4.2/Source/CMSIS_RTOS/cmsis_os.c
+++ HC32F072_FreeRTOS/freertos-7.4.2/Source/CMSIS_RTOS/cmsis_os.c
@@ -33,26 +33,6 @@
   * @date    03-March-2017
   * @brief   CMSIS-RTOS API implementation for FreeRTOS V9.0.0
   ******************************************************************************
-  * @attention
-  *
-  * Redistribution and use in source and binary forms, with or without 
-  * modification, are permitted, provided that the following conditions are met:
-  *
-  * 1. Redistribution of source code must retain the above copyright notice, 
-  *    this list of conditions and the following disclaimer.
-  * 2. Redistributions in binary form must reproduce the above copyright notice,
-  *    this list of conditions and the following disclaimer in the documentation
-  *    and/or other materials provided with the distribution.
-  * 3. Neither the name of HDSC nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
-  *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
-  *    software, must execute solely and exclusively on microcontroller or
-  *    microprocessor devices manufactured by or for HDSC
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
-  *
   * 
   *
   ******************************************************************************
--- HC32F072_FreeRTOS/freertos-7.4.2/Source/CMSIS_RTOS/cmsis_os.h
+++ HC32F072_FreeRTOS/freertos-7.4.2/Source/CMSIS_RTOS/cmsis_os.h
@@ -35,25 +35,6 @@
   *          A new set of APIs are added in addition to existing ones, these APIs 
   *          are specific to FreeRTOS.
   ******************************************************************************
-  * @attention
-  *
-  * Redistribution and use in source and binary forms, with or without 
-  * modification, are permitted, provided that the following conditions are met:
-  *
-  * 1. Redistribution of source code must retain the above copyright notice, 
-  *    this list of conditions and the following disclaimer.
-  * 2. Redistributions in binary form must reproduce the above copyright notice,
-  *    this list of conditions and the following disclaimer in the documentation
-  *    and/or other materials provided with the distribution.
-  * 3. Neither the name of HDSC nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
-  *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
-  *    software, must execute solely and exclusively on microcontroller or
-  *    microprocessor devices manufactured by or for HDSC.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
   *
   * 
   *
